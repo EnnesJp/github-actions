14d3b10583606453c4c2b53c006a30e03f933a92
João Pedro Ennes <65932730+EnnesJp@users.noreply.github.com>
2023-06-02 02:41:10 -0300

    try to fix tests

--- tests/TarefaTest.java
+++ tests/TarefaTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
-
+import org.junit.platform.runner.JUnitPlatform;
 @SuppressWarnings({ "unused", "deprecation" })
 class TarefaTest {
 
